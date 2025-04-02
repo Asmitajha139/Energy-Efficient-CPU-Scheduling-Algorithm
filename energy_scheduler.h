e30420d079216763885c7a09b367f7e64c233a82
Asmitajha139 <151938400+Asmitajha139@users.noreply.github.com>
2025-04-02 23:54:21 +0530

    Update energy_scheduler.h

--- energy_scheduler.h
+++ energy_scheduler.h
@@ -1,22 +1,23 @@
 #ifndef ENERGY_SCHEDULER_H
 #define ENERGY_SCHEDULER_H
-#define MAX_TASKS 100  
+#define MAX_TASKS 100
 typedef struct {
-    int task_id;        
+    int task_id;         
     int exec_time;       
     int arrival_time;    
     int priority;        
     int completed;      
+} Task;
 
 typedef struct {
     Task tasks[MAX_TASKS];  
     int task_count;        
-    int total_energy;       
-    int current_time;      
+    int total_energy;      
+    int current_time;       
 } Scheduler;
-void init_scheduler(Scheduler *sched);              
-void add_task(Scheduler *sched, int id, int exec_time, int arrival_time, int priority);  
-void schedule_tasks(Scheduler *sched);              
-void print_schedule(Scheduler *sched);              
 
+void init_scheduler(Scheduler *sched);
+int add_task(Scheduler *sched, int id, int exec_time, int arrival_time, int priority);
+void schedule_tasks(Scheduler *sched);
+void print_schedule(Scheduler *sched);
 #endif
